c5c11aec46bae3c8d899e6727cbc4a4a18cf4df1
Erion Berdynaj <erion.berdyna@hotmail.com>
2023-05-05 23:49:28 +0200

    BACKEND PRODUCT

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,96 @@
+﻿using Another_test.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Another_test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : Controller
+    {
+        private readonly PharmacyDbContext _productDbContext;
+
+        public ProductController(PharmacyDbContext productDbContext)
+        {
+            _productDbContext = productDbContext;
+        }
+
+        //[HttpGet]
+        //[Route("GetProduct")]
+        //public async Task<IEnumerable<Product>> GetProducts()
+        //{
+        //    // get all products from the context
+        //    var products = await _productDbContext.Products.ToListAsync();
+
+        //    // loop through each product and modify its Name property
+        //    foreach (var product in products)
+        //    {
+        //        product.Name += "test";
+        //    }
+
+        //    // save changes to the database
+        //    await _productDbContext.SaveChangesAsync();
+
+        //    // return all products, including the modified ones
+        //    return products;
+        //}
+
+        [HttpGet]
+        [Route("GetProduct")]
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _productDbContext.Products.ToListAsync();
+        }
+
+        [HttpPost]
+        [Route("addProduct")]
+
+        public async Task<Product> AddProduct(Product objProduct/*, IFormFile photo*/)
+        {
+            //if (photo != null && photo.Length > 0)
+            //{
+            //    using (var stream = new MemoryStream())
+            //    {
+            //        await photo.CopyToAsync(stream);
+            //        objProduct.ImgUrl = stream.ToArray();
+            //    }
+            //}
+            _productDbContext.Products.Add(objProduct);
+            await _productDbContext.SaveChangesAsync();
+            return objProduct;
+        }
+        [HttpPatch]
+        [Route("UpdateProduct/{id}")]
+
+        public async Task<Product> UpdateProduct(Product objProduct)
+        {
+            _productDbContext.Entry(objProduct).State = EntityState.Modified;
+            await _productDbContext.SaveChangesAsync();
+            return objProduct;
+        }
+
+
+        [HttpDelete]
+        [Route("DeleteProduct/{id}")]
+
+        public bool DeleteProduct(int id)
+        {
+            bool a = false;
+            var product = _productDbContext.Products.Find(id);
+            if (product != null)
+            {
+                a = true;
+                _productDbContext.Entry(product).State = EntityState.Deleted;
+                _productDbContext.SaveChanges();
+            }
+            else
+            {
+                a = false;
+            }
+
+            return a;
+        }
+
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Another_test.Models
+{
+    public class Product
+    {
+        [Key]
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string description { get; set; } 
+        public int price { get; set; }  
+        public int quantity { get; set; }   
+        public string ImgUrl { get; set; }
+    }
+}
