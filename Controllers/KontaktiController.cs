daaad35cc7bdac3eec1462e95d4afdc0c57e00d4
Venera Gxhufi <venera.goxhufi12@gmail.com>
2023-05-05 23:53:03 +0200

    Contact-API

---
+++ Controllers/KontaktiController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pharmacy.Models;
+
+namespace Pharmacy.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KontaktiController : ControllerBase
+    {
+        private readonly PharmacyDbContext _kontaktiDbContext;
+
+        public KontaktiController(PharmacyDbContext kontaktiDbContext)
+        {
+            _kontaktiDbContext = kontaktiDbContext;
+        }
+
+        [HttpGet]
+        [Route("GetKontakti")]
+        public async Task<IEnumerable<Kontakti>> GetKontakts()
+        {
+            return await _kontaktiDbContext.Kontakti.ToListAsync();
+        }
+        [HttpPost]
+        [Route("AddKontakti")]
+        public async Task<Kontakti> AddKontakti(Kontakti objKontakti)
+        {
+            _kontaktiDbContext.Kontakti.Add(objKontakti);
+            await _kontaktiDbContext.SaveChangesAsync();
+            return objKontakti;
+        }
+        [HttpPatch]
+        [Route("UpdateKontakti/{id}")]
+        public async Task<Kontakti> UpdateKontakti(Kontakti objKontakti)
+        {
+            _kontaktiDbContext.Entry(objKontakti).State = EntityState.Modified;
+            await _kontaktiDbContext.SaveChangesAsync();
+            return objKontakti;
+        }
+
+        [HttpDelete]
+        [Route("DeleteKontakti/{id}")]
+
+        public bool DeleteKontakti(int id)
+        {
+            bool a = false;
+            var kontakti = _kontaktiDbContext.Kontakti.Find(id);
+
+            if (kontakti != null)
+            {
+                a = true;
+                _kontaktiDbContext.Entry(kontakti).State = EntityState.Deleted;
+                _kontaktiDbContext.SaveChanges();
+            }
+            else
+            {
+                a = false;
+            }
+            return a;
+        }
+    }
+}
+       
+
